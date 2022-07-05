f3c2b566e909458032032a30ebf5a8bc9c3b1b53
GuiLeoni14 <guileone167@gmail.com>
2022-07-05 20:35:05 -0300

    criando pagina de subscribe

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ['./src/**/*.tsx'],
     theme: {
         extend: {
+            backgroundImage: {
+                blur: 'url(/src/assets/img/blur_background.png)',
+            },
             fontFamily: {
                 sans: 'Roboto, san-serif',
             },
